25fa55ca3fce8038ac20b095ad8c17394bbc1568
Thierry Ducrest <thierry@ducrest.net>
2024-11-04 12:12:44 +0100

    fixup! Add purchase_propagate_qty_mrp

--- purchase_propagate_qty_mrp/models/purchase.py
+++ purchase_propagate_qty_mrp/models/purchase.py
@@ -9,24 +9,23 @@ class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     def _propagage_qty_to_moves(self):
-        kit_line = self.env["purchase.order.line"].browse()
+        kit_purchase_lines = self.env["purchase.order.line"].browse()
         for line in self:
-            if line.product_id.bom_ids:
-                kit_line |= line
-                line._propagate_qty_to_moves_mrp()
-
-        super(PurchaseOrderLine, self - kit_line)._propagage_qty_to_moves()
+            if line.state != "purchase":
+                continue
+            kit_line = line._propagate_qty_to_moves_mrp()
+            if kit_line:
+                kit_purchase_lines |= line
+        super(PurchaseOrderLine, self - kit_purchase_lines)._propagage_qty_to_moves()
 
     def _propagate_qty_to_moves_mrp(self):
         self.ensure_one()
-        # Take the first bom, how to know if it is the correct one ?
-        #    There is _bom_find
+        # Take the first bom, how to know if it is the correct one ? There is _bom_find, maybe ?
         bom = self.product_id.bom_ids[0]
         if not bom:
-            return
+            return None
         new_kit_quantity = self.product_uom_qty
-        # Get quantity done from mrp
-        # WHY not used the qty done on the purchase line ?
+        # Get quantity done from mrp, why not used the qty_received on the purchase line ?
         moves_done = self.move_ids.filtered(
             lambda move: move.state == "done" and not move.scrapped
         )
@@ -42,7 +41,7 @@ class PurchaseOrderLine(models.Model):
         done_kit_quantity = moves_done._compute_kit_quantities(
             self.product_id, new_kit_quantity, bom, filters
         )
-        if done_kit_quantity < new_kit_quantity:
+        if done_kit_quantity <= new_kit_quantity:
             boms, bom_sub_lines = bom.explode(self.product_id, new_kit_quantity)
             for bom_line, bom_line_data in bom_sub_lines:
                 bom_line_uom = bom_line.product_uom_id
@@ -52,10 +51,18 @@ class PurchaseOrderLine(models.Model):
                 component_qty, procurement_uom = bom_line_uom._adjust_uom_quantities(
                     bom_line_data["qty"], quant_uom
                 )
-                move = self.move_ids.filtered(
+                moves = self.move_ids.filtered(
                     lambda move: move.product_id == bom_line.product_id
-                    and move.state not in ("done", "cancel")
                 )
-                move.product_uom_qty = component_qty
+
+                moves_not_canceled = moves.filtered(lambda r: r.state not in ("cancel"))
+                previous_qty = sum(moves.mapped("product_uom_qty"))
+
+                removable_qty = moves._get_removable_qty()
+
+                quantity_to_remove = previous_qty - component_qty
+                if removable_qty >= quantity_to_remove:
+                    moves._deduce_qty(quantity_to_remove, procurement_uom.id)
         else:
             raise UserError(_("You cannot remove more that what remains to be done."))
+        return self
--- purchase_propagate_qty_mrp/tests/test_purchase_delivery.py
+++ purchase_propagate_qty_mrp/tests/test_purchase_delivery.py
@@ -16,18 +16,7 @@ class TestQtyUpdate(SavepointCase):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.product_model = cls.env["product.product"]
-
-        # Create products:
-        cls.p1 = cls.product1 = cls.product_model.create(
-            {"name": "Test Product 1", "type": "product", "default_code": "PROD1"}
-        )
-        p2 = cls.product2 = cls.product_model.create(
-            {"name": "Test Product 2", "type": "product", "default_code": "PROD2"}
-        )
-
-        partner = cls.env["res.partner"].create({"name": "supplier"})
-        cls.seller = partner
-
+        cls.seller = cls.env["res.partner"].create({"name": "supplier"})
         # Create a kit
         cls.product_bed_with_curtain = cls.product_model.create(
             {
@@ -37,7 +26,6 @@ class TestQtyUpdate(SavepointCase):
                 "purchase_ok": False,
             }
         )
-        cls.tmpl_bed_with_curtain = cls.product_bed_with_curtain.product_tmpl_id
         cls.product_bed_structure = cls.product_model.create(
             {
                 "name": "BED STRUCTURE",
@@ -45,7 +33,6 @@ class TestQtyUpdate(SavepointCase):
                 "sale_ok": True,
                 "purchase_ok": True,
                 "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 50.0})],
-                # "route_ids": [(4, cls.mto_route.id), (4, cls.buy_route.id)],
             }
         )
         cls.product_bed_curtain = cls.product_model.create(
@@ -55,13 +42,12 @@ class TestQtyUpdate(SavepointCase):
                 "sale_ok": True,
                 "purchase_ok": True,
                 "seller_ids": [(0, 0, {"name": cls.seller.id, "price": 10.0})],
-                # "route_ids": [(4, cls.buy_route.id)],
             }
         )
         cls.bom_model = cls.env["mrp.bom"]
         cls.bom_bed_with_curtain = cls.bom_model.create(
             {
-                "product_tmpl_id": cls.tmpl_bed_with_curtain.id,
+                "product_tmpl_id": cls.product_bed_with_curtain.product_tmpl_id.id,
                 "product_id": cls.product_bed_with_curtain.id,
                 "type": "phantom",
                 "bom_line_ids": [
@@ -85,7 +71,7 @@ class TestQtyUpdate(SavepointCase):
         cls.date_planned = "2020-04-30 12:00:00"
         cls.po = cls.env["purchase.order"].create(
             {
-                "partner_id": partner.id,
+                "partner_id": cls.seller.id,
                 "order_line": [
                     (
                         0,
@@ -99,42 +85,6 @@ class TestQtyUpdate(SavepointCase):
                             "price_unit": 300.0,
                         },
                     ),
-                    # (
-                    #     0,
-                    #     0,
-                    #     {
-                    #         "product_id": cls.p1.id,
-                    #         "product_uom": cls.p1.uom_id.id,
-                    #         "name": cls.p1.name,
-                    #         "date_planned": cls.date_planned,
-                    #         "product_qty": 42.0,
-                    #         "price_unit": 10.0,
-                    #     },
-                    # ),
-                    # (
-                    #     0,
-                    #     0,
-                    #     {
-                    #         "product_id": p2.id,
-                    #         "product_uom": p2.uom_id.id,
-                    #         "name": p2.name,
-                    #         "date_planned": cls.date_planned,
-                    #         "product_qty": 12.0,
-                    #         "price_unit": 10.0,
-                    #     },
-                    # ),
-                    # (
-                    #     0,
-                    #     0,
-                    #     {
-                    #         "product_id": cls.p1.id,
-                    #         "product_uom": cls.p1.uom_id.id,
-                    #         "name": cls.p1.name,
-                    #         "date_planned": cls.date_planned,
-                    #         "product_qty": 1.0,
-                    #         "price_unit": 10.0,
-                    #     },
-                    # ),
                 ],
             }
         )
@@ -167,77 +117,52 @@ class TestQtyUpdate(SavepointCase):
         new_move_2._action_done()
 
     def test_purchase_line_qty_decrease_allowed(self):
-        """"""
-        line1 = self.po.order_line[0]
-        moves = line1.move_ids
-        line1.write({"product_qty": 30})
+        """Check decreasing ordered quantity to less than what is left to receive.
+
+        Purchased 42 kits
+        Received nothing yet
+        Update the purchase quantity to 25
+
+        """
+        po_line = self.po.order_line[0]
+        moves = po_line.move_ids
+        po_line.write({"product_qty": 25})
         self._check_moves(
-            moves, {self.product_bed_structure: 30, self.product_bed_curtain: 60}
+            moves, {self.product_bed_structure: 25, self.product_bed_curtain: 50}
         )
 
     def test_purchase_line_qty_decrease_not_allowed(self):
-        """"""
-        line1 = self.po.order_line[0]
-        self._receive_kit(line1, 30)
-        moves = line1.move_ids
+        """Check decreasing the quantity to more than what is left to receive.
+
+        Purchased 42 kits
+        Receive 30 of them
+        Update the purchase quantity to 25 -> not allowed
+
+        """
+        po_line = self.po.order_line[0]
+        self._receive_kit(po_line, 30)
+        moves = po_line.move_ids
         self._check_moves(
             moves, {self.product_bed_structure: 12, self.product_bed_curtain: 24}
         )
         with self.assertRaises(UserError):
-            line1.write({"product_qty": 30})
+            po_line.write({"product_qty": 25})
 
-    # def test_purchase_line_unlink(self):
-    #     """decrease qty on confirmed po -> decreased reception"""
-    #     line1 = self.po.order_line[0]
-    #     exception_regex = (
-    #         r"Cannot delete a purchase order line which is in state 'purchase'."
-    #     )
-    #     with self.assertRaisesRegex(UserError, exception_regex):
-    #         line1.unlink()
+    def test_decrease_purchase_qty_nothing_left_to_receive(self):
+        """Check decreasing the quantity to the exact amount that has been received.
 
-    # def test_purchase_line_qty_decrease_to_zero(self):
-    #     """decrease qty on confirmed po -> decreased reception"""
-    #     line1 = self.po.order_line[0]
-    #     move1 = self.env["stock.move"].search([("purchase_line_id", "=", line1.id)])
-    #     line1.write({"product_qty": 0})
-    #     self.assertEqual(move1.product_uom_qty, 0)
-    #     self.assertEqual(move1.state, "cancel")
+        Purchased 42 kits
+        Received 30
+        Update the quantity on the purchase to 30
 
-    # def test_reduce_purchase_qty_with_canceled_moves(self):
-    #     """Check canceled moves are not taken into account."""
-    #     self.po.button_cancel()  # Cancel the moves
-    #     self.po.button_draft()
-    #     self.po.button_confirm()
-    #     line1 = self.po.order_line[0]
-    #     # One canceled move one ready
-    #     self.assertEqual(len(line1.move_ids), 2)
-    #     move_canceled = line1.move_ids.filtered_domain([("state", "=", "cancel")])
-    #     move_assigned = line1.move_ids.filtered_domain([("state", "=", "assigned")])
-    #     self.assertEqual(len(move_canceled), 1)
-    #     self.assertEqual(len(move_assigned), 1)
-    #     line1.write({"product_qty": 10})
-    #     move = line1.move_ids.filtered(lambda r: r.state != "cancel")
-    #     self.assertEqual(move.product_uom_qty, 10)
-
-    # def test_reduce_purchase_qty_with_done_moves(self):
-    #     """Check canceled moves are not taken into account."""
-    #     self.po.button_draft()
-    #     self.po.button_confirm()
-    #     line1 = self.po.order_line[0]
-    #     self.assertEqual(len(line1.move_ids), 1)
-    #     # Receiving a partial qty (here we split the existing move by
-    #     # convenience but we could generate a backorder as well)
-    #     new_move_vals = line1.move_ids._split(10)
-    #     new_move = line1.move_ids.create(new_move_vals)
-    #     new_move._action_confirm(merge=False)
-    #     new_move._action_assign()
-    #     new_move.quantity_done = 10
-    #     new_move._action_done()  # Receive 10/42 qties
-    #     # Update the purchase order line to fit the received qty
-    #     #   => the remaining 32 qties have to be canceled
-    #     line1.write({"product_qty": 10})
-    #     move_canceled = line1.move_ids.filtered_domain([("state", "=", "cancel")])
-    #     move_done = line1.move_ids.filtered_domain([("state", "=", "done")])
-    #     self.assertEqual(move_canceled.product_uom_qty, 32)
-    #     self.assertEqual(move_done.product_uom_qty, 10)
-    #     self.assertEqual(sum(line1.move_ids.mapped("product_uom_qty")), 42)
+        """
+        po_line = self.po.order_line[0]
+        self._receive_kit(po_line, 30)
+        moves = po_line.move_ids
+        self._check_moves(
+            moves, {self.product_bed_structure: 12, self.product_bed_curtain: 24}
+        )
+        po_line.write({"product_qty": 30})
+        self._check_moves(
+            moves, {self.product_bed_structure: 0, self.product_bed_curtain: 0}
+        )
